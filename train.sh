401f65ff7b7ee2e3864c17cd3dcb5b035b003a2a
lpupp <lucagaegauf@Lucas-MacBook-Pro.local>
2019-05-15 11:50:44 +0200

    typo

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=168:00:00
 #SBATCH --gres gpu:Tesla-K80:1
 
-BASE_PATH=/home/cluster/lgaega/
+BASE_PATH=/home/cluster/lgaega
 GIT_PATH=${BASE_PATH}/PyTorch-CycleGAN
 DATA_BASE_PATH=${BASE_PATH}/data/CycleGAN
 
@@ -29,4 +29,4 @@ IMG_SIZE=256
 BS=1
 LR=0.0002
 
-srun python ${GIT_PATH}/training.py --dataroot ${DATA_BASE_PATH}/${RUN_DOMAIN}/${RUN_NAME} --cuda --n_cpu 8 --n_epochs 300 --batch_size ${BS} --lr ${LR} --size ${IMG_SIZE} --output_dir ${OUTPUT_DIR} >> ${OUTPUT_DIR}/${RUN_NAME}_${IMG_SIZE}_${BS}_${LR}.txt
+srun python ${GIT_PATH}/train.py --dataroot ${DATA_BASE_PATH}/${RUN_DOMAIN}/${RUN_NAME} --cuda --n_cpu 8 --n_epochs 300 --batch_size ${BS} --lr ${LR} --size ${IMG_SIZE} --output_dir ${OUTPUT_DIR} >> ${OUTPUT_DIR}/${RUN_NAME}_${IMG_SIZE}_${BS}_${LR}.txt
